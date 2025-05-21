2c5476e8ca37f97bbb1b79fbbe8dc1b9780dcf71
oger-gabriel <168653684+oger-gabriel@users.noreply.github.com>
2025-05-21 09:22:39 +0200

    Partie 2 : CRUD des ingrédients - Modier un ingrédient

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -51,4 +51,25 @@ final class IngredientController extends AbstractController
             "form" => $form,
         ]);
     }
+
+    #[Route("/ingredient/edit/{id}", "ingredient_edit", methods: ["GET", "POST"])]
+    public function edit(Request $request, EntityManagerInterface $manager, Ingredient $ingredient): Response
+    {
+        $form = $this->createForm(IngredientType::class, $ingredient);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ingredient = $form->getData();
+            //$manager->persist($ingredient);
+            $manager->flush();
+
+            $this->addFlash("success", "Vos changements ont été enregistrés !");
+
+            return $this->redirectToRoute("app_ingredient");
+        }
+
+        return $this->render("pages/ingredient/edit.html.twig", [
+            "form" => $form,
+        ]);
+    }
 }
